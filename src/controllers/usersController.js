dea8a00297681cf506e946d2e85580de0270b2b1
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-02 10:23:00 -0300

    creating sessions

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -40,7 +40,7 @@ class UsersController {
     }
 
     user.name = name ?? user.name
-    user.email = email ?? user.name
+    user.email = email ?? user.email
 
     if(password && !old_password) {
       throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
