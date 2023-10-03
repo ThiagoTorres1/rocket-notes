aba1307a7aa437a2f33ec2cd0bd5ad763a41c01f
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-03 14:59:39 -0300

    image upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,19 @@
 const {Router} = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/usersController")
+const UserAvatarController = require("../controllers/userAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const userController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post("/", userController.create)
 usersRoutes.put("/", ensureAuthenticated, userController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
