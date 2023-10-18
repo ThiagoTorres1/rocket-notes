b02a7043872c6ce0baaf8da9a90f63be10dbbb8a
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-18 14:10:54 -0300

    jest

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,22 +2,17 @@ const {hash, compare} = require("bcryptjs")
 
 const AppError = require("../utils/AppError")
 
+const UserRepository = require("../repositories/userRepository")
+const UserCreateService = require("../services/userCreateService")
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response) {
     const {name, email, password} = request.body
-
-    const database = await sqliteConnection()
-    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-
-    if(checkUserExist) {
-      throw new AppError('Este e-mail já está cadastrado.')
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+    
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
+    await userCreateService.execute({name, email, password})
 
     return response.status(201).json();
   }
